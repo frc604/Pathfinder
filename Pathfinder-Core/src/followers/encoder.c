7053349286f68877c74e33d67be6c659847b4304
rlee287 <rlee287@yahoo.com>
2018-07-07 14:29:06 -0700

    UNTESTED port of PID continuation logic to C

--- Pathfinder-Core/src/followers/encoder.c
+++ Pathfinder-Core/src/followers/encoder.c
@@ -4,13 +4,13 @@ double pathfinder_follow_encoder(EncoderConfig c, EncoderFollower *follower, Seg
     int segment = follower->segment;
     if (segment >= trajectory_length) {
         follower->finished = 1;
-        follower->output = 0.0;
+        // follower->output = 0.0;
         Segment last = trajectory[trajectory_length - 1];
         follower->heading = last.heading;
-        return 0.0;
-    } else {
+        // return 0.0;
+    } // else {
         return pathfinder_follow_encoder2(c, follower, trajectory[segment], trajectory_length, encoder_tick);
-    }
+    // }
 }
 
 double pathfinder_follow_encoder2(EncoderConfig c, EncoderFollower *follower, Segment s, int trajectory_length, int encoder_tick) {
@@ -19,7 +19,11 @@ double pathfinder_follow_encoder2(EncoderConfig c, EncoderFollower *follower, Se
     
     if (follower->segment < trajectory_length) {
         follower->finished = 0;
-        double error = s.position - distance_covered;
+    } else {
+        follower->finished = 1;
+        // return 0.0;
+    }
+    double error = s.position - distance_covered;
         double calculated_value = c.kp * error +
                                   c.kd * ((error - follower->last_error) / s.dt) +
                                   (c.kv * s.velocity + c.ka * s.acceleration);
@@ -29,8 +33,4 @@ double pathfinder_follow_encoder2(EncoderConfig c, EncoderFollower *follower, Se
         follower->output = calculated_value;
         follower->segment = follower->segment + 1;
         return calculated_value;
-    } else {
-        follower->finished = 1;
-        return 0.0;
-    }
 }
