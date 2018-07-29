10c2a18729161270c36268a40ee478ac15205692
InfinityPhase <4a.zanella@gmail.com>
2018-07-28 23:12:11 -0700

    Added checking of max velocity to tank path generation

--- Pathfinder-Core/src/modifiers/tank.c
+++ Pathfinder-Core/src/modifiers/tank.c
@@ -1,7 +1,11 @@
 #include "pathfinder.h"
 
-void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, Segment *right_traj, double wheelbase_width) {
+void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, Segment *right_traj, double wheelbase_width, TrajectoryConfig config) {
     double w = wheelbase_width / 2;
+    
+    // Used to keep track of added time to path
+    double dt_offset_left = 0;
+    double dt_offset_right = 0;
     
     int i;
     for (i = 0; i < length; i++) {
@@ -22,8 +26,19 @@ void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, S
                 + (left.y - last.y) * (left.y - last.y)
             );
             
+            // We need to offset the rest of the path too
+            left.dt = seg.dt + dt_offset_left;
+            
             left.position = last.position + distance;
             left.velocity = distance / seg.dt;
+            
+            // Check if velocity is over max
+            if (left.velocity > config.max_v ) {
+                left.dt = (distance / config.max_v) + dt_offset_left;
+                left.velocity = config.max_v;
+                dt_offset_left += left.dt - seg.dt;
+            }
+            
             left.acceleration = (left.velocity - last.velocity) / seg.dt;
             left.jerk = (left.acceleration - last.acceleration) / seg.dt;
         }
@@ -38,8 +53,17 @@ void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, S
                 + (right.y - last.y) * (right.y - last.y)
             );
             
+            right.dt = seg.dt + dt_offset_right;
+            
             right.position = last.position + distance;
             right.velocity = distance / seg.dt;
+            
+            if (right.velocity > config.max_v ) {
+                right.dt = (distance / config.max_v) + dt_offset_right;
+                right.velocity = config.max_v;
+                dt_offset_right += right.dt - seg.dt;
+            }
+            
             right.acceleration = (right.velocity - last.velocity) / seg.dt;
             right.jerk = (right.acceleration - last.acceleration) / seg.dt;
         }
