c4707e2fda41b9f9887e38c39bf3bfbafd8eda1b
InfinityPhase <InfinityPhase@users.noreply.github.com>
2018-07-30 16:00:33 -0700

    It is unnessessary to keep track of dt changes

--- Pathfinder-Core/src/modifiers/tank.c
+++ Pathfinder-Core/src/modifiers/tank.c
@@ -3,10 +3,6 @@
 void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, Segment *right_traj, double wheelbase_width, double max_v) {
     double w = wheelbase_width / 2;
     
-    // Used to keep track of added time to path
-    double dt_offset_left = 0;
-    double dt_offset_right = 0;
-    
     int i;
     for (i = 0; i < length; i++) {
         Segment seg = original[i];
@@ -25,18 +21,14 @@ void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, S
                 (left.x - last.x) * (left.x - last.x)
                 + (left.y - last.y) * (left.y - last.y)
             );
-            
-            // We need to offset the rest of the path too
-            left.dt = seg.dt + dt_offset_left;
-            
+                        
             left.position = last.position + distance;
             left.velocity = distance / seg.dt;
             
             // Check if velocity is over max
             if (left.velocity > max_v ) {
-                left.dt = (distance / max_v) + dt_offset_left;
+                left.dt = distance / max_v;
                 left.velocity = max_v;
-                dt_offset_left += left.dt - seg.dt;
             }
             
             left.acceleration = (left.velocity - last.velocity) / seg.dt;
@@ -53,15 +45,14 @@ void pathfinder_modify_tank(Segment *original, int length, Segment *left_traj, S
                 + (right.y - last.y) * (right.y - last.y)
             );
             
-            right.dt = seg.dt + dt_offset_right;
+            right.dt = seg.dt;
             
             right.position = last.position + distance;
             right.velocity = distance / seg.dt;
             
             if (right.velocity > max_v ) {
-                right.dt = (distance / max_v) + dt_offset_right;
+                right.dt = distance / max_v;
                 right.velocity = max_v;
-                dt_offset_right += right.dt - seg.dt;
             }
             
             right.acceleration = (right.velocity - last.velocity) / seg.dt;
