ec2cfd278af80d94632ef6346cdef157630b0d9b
rlee287 <rlee287@yahoo.com>
2018-08-02 18:03:58 -0700

    OS X install gettext for envsubst

--- travis_install.sh
+++ travis_install.sh
@@ -2,4 +2,8 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-add-repository ppa:wpilib/toolchain -y
   sudo apt-get update -q
   sudo apt-get install frc-toolchain -y
+else # OS X
+  # Install envsubst
+  brew install getteext
+  brew link --force gettext
 fi
