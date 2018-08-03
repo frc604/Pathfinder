e278da9530e8663c29d7a597e7294fad32b79a2c
rlee287 <rlee287@yahoo.com>
2018-08-02 17:58:55 -0700

    Try to figure out SFTP failues on OS X

--- travis_deploy.sh
+++ travis_deploy.sh
@@ -3,10 +3,10 @@ chmod 600 upload_key
 eval $(ssh-agent -s) > /dev/null
 ssh-add upload_key > /dev/null
 envsubst < upload_sftp > upload_sftp_subst
+cat upload_sftp_subst
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
   export STRICT_CHECK='-oStrictHostKeyChecking=accept-new'
 fi
-sftp $STRICT_CHECK -b upload_sftp_subst -i upload_key -P 2222 $sftp_user
-else
+sftp -v $STRICT_CHECK -b upload_sftp_subst -i upload_key -P 2222 $sftp_user
 eval $(ssh-agent -k) > /dev/null
 rm upload_key
