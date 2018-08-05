808f50bedce56d5ca8dd2774da03ab44bb1aec71
rlee287 <rlee287@yahoo.com>
2018-08-05 10:31:43 -0700

    Try list to reduce time for update

--- travis_install.sh
+++ travis_install.sh
@@ -1,5 +1,6 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-add-repository ppa:wpilib/toolchain -y
+  ls /etc/apt/sources.list.d
   sudo apt-get update -q
   sudo apt-get install frc-toolchain -y
 else # OS X
