f256660f354ec736be0c96f73d127ffecca6d678
rlee287 <rlee287@yahoo.com>
2018-08-05 10:52:08 -0700

    Keep trying update/grade

--- travis_install.sh
+++ travis_install.sh
@@ -2,7 +2,7 @@ if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-add-repository ppa:wpilib/toolchain -y
   ls /etc/apt/sources.list.d
   sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/wpilib-toolchain-trusty.list -q
-  sudo apt-get install frc-toolchain -y
+  sudo apt-get install frc-toolchain gdb-arm-frc-linux-gnueabi frcmake -y
 else # OS X
   # Link envsubst
   brew link --force gettext
