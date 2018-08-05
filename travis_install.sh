b16af95d45cb8858bc34b0a2d690e31a9afe9d21
rlee287 <rlee287@yahoo.com>
2018-08-05 10:38:11 -0700

    Try only updating from wpilib repository

--- travis_install.sh
+++ travis_install.sh
@@ -1,7 +1,7 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-add-repository ppa:wpilib/toolchain -y
   ls /etc/apt/sources.list.d
-  sudo apt-get update -q
+  sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/wpilib-toolchain-trusty.list -q
   sudo apt-get install frc-toolchain -y
 else # OS X
   # Link envsubst
