e3c31f4c45fe0409488885b9c931767dffdfe94e
rlee287 <rlee287@yahoo.com>
2018-08-05 11:02:54 -0700

    Continue tests by listing sources.list

--- travis_install.sh
+++ travis_install.sh
@@ -1,8 +1,9 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-add-repository ppa:wpilib/toolchain -y
   ls /etc/apt/sources.list.d
+  cat /etc/apt/sources.list
   sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/wpilib-toolchain-trusty.list -q
-  sudo apt-get install frc-toolchain gdb-arm-frc-linux-gnueabi frcmake -y
+  sudo apt-get install frc-toolchain -y
 else # OS X
   # Link envsubst
   brew link --force gettext
