caece958e3e3fcc107c4789c87a236d2d53d4076
rlee287 <rlee287@yahoo.com>
2018-08-05 11:21:01 -0700

    Reset back to full update
    May try exhausitve listing later

--- travis_install.sh
+++ travis_install.sh
@@ -1,10 +1,6 @@
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
   sudo apt-add-repository ppa:wpilib/toolchain -y
-  echo "list sources.list.d"
-  ls /etc/apt/sources.list.d
-  echo "cat sources.list"
-  cat /etc/apt/sources.list
-  sudo apt-get update -o Dir::Etc::sourcelist=/etc/apt/sources.list.d/wpilib-toolchain-trusty.list -q
+  sudo apt-get update -q
   sudo apt-get install frc-toolchain -y
 else # OS X
   # Link envsubst
