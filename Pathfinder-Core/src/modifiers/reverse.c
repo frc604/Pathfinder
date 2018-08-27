1591fb48da94623209017b444c4f5775724d271b
Ryan Lee <rlee287@yahoo.com>
2018-08-27 11:34:44 -0700

    Comment out debug prints in the reverse function
    Can revert locally for testing but printing should not be in the production library

--- Pathfinder-Core/src/modifiers/reverse.c
+++ Pathfinder-Core/src/modifiers/reverse.c
@@ -3,7 +3,7 @@
 void pathfinder_modify_reverse(Segment *original, int length, Segment *dest) {
     int i;
     for( i = 0; i < length; ++i ) {
-        printf("%d POS: %f X: %f Y: %f\n", i, original[i].position, original[i].x, original[i].y);
+        // printf("%d POS: %f X: %f Y: %f\n", i, original[i].position, original[i].x, original[i].y);
         dest[i].position = original[i].position * -1;
         dest[i].velocity = original[i].velocity * -1;
         dest[i].acceleration = original[i].acceleration * -1;
@@ -11,8 +11,8 @@ void pathfinder_modify_reverse(Segment *original, int length, Segment *dest) {
         dest[i].heading = original[i].heading;
         dest[i].x = original[i].x;
         dest[i].y = original[i].y;
-        printf("%d POS: %f X: %f Y: %f\n", i, dest[i].position, dest[i].x, dest[i].y);
+        // printf("%d POS: %f X: %f Y: %f\n", i, dest[i].position, dest[i].x, dest[i].y);
 
     }
-    printf("\n");
+    // printf("\n");
 }
